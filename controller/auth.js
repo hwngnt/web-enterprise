983d9123f72b38654f63f0ab15c88b14b2fd361b
Nguyen Thi Lan Anh <81799232+lananh20111999@users.noreply.github.com>
2022-02-28 18:36:22 +0700

    ok

--- controller/auth.js
+++ controller/auth.js
@@ -4,36 +4,17 @@ const bcrypt = require('bcryptjs');
 exports.handleLogin = async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    let courses = [];
+
     try {
         let user = await Account.findOne({ email: username });
         await bcrypt.compare(password, user.password).then((doMatch) => {
             if (doMatch) {
                 
-                if (user.Role == 'admin') {
+                if (user.role == 'admin') {
                     req.session.user = user;
                     req.session.email = username;
                     req.session.admin = true;
                     res.redirect('/admin');
-                }
-                else if (user.Role == 'staff') {
-                    req.session.user = user;
-                    req.session.email = username;
-                    req.session.staff = true;
-                    res.redirect('/staff');
-                }
-                else if (user.Role == 'trainer') {
-                    req.session.user = user;
-                    req.session.email = username;
-                    req.session.trainer = true;
-                    res.redirect('/trainer');
-                }
-                else {
-                    req.session.user = user;
-                    req.session.email = username;
-                    req.session.trainee = true;
-                    req.session.courses = courses;
-                    res.redirect('/trainee');
                 }
             } else {
                 return res.render('index', { errors: 'Username or password is incorrect' })
@@ -44,8 +25,8 @@ exports.handleLogin = async (req, res) => {
             //     console.log(err)
             // })
     } catch (error) {
-        //console.log(error);
-        //return res.render('index');
+        console.log(error);
+        return res.render('index');
     }
 };
 
--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const multer = require('multer');
 const mongoose = require('mongoose');
 const adminController = require('../controller/admin');
-// const { isAdmin } = require("../middleware/auth");
+const { isAdmin } = require("../middleware/auth");
 
-router.get('/admin', adminController.getAdmin);
+router.get('/admin', isAdmin, adminController.getAdmin);
+module.exports = router;
