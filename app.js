663ce4854710e9023988d1c9f6e518b7fa66a5d5
Nguyen Thi Lan Anh <81799232+lananh20111999@users.noreply.github.com>
2022-02-28 15:10:31 +0700

    ok

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('./db/db');
+const bodyParser = require('body-parser');
+session = require('express-session')
+const app = express();
+app.set('view engine', 'hbs');
+var hbs = require('hbs');
+const staff = require('./models/staff');
+app.use(bodyParser.urlencoded({ extended: true }));
+// register path to partials
+hbs.registerPartials(__dirname + '/views/partials/');
+// hbs.registerHelper('dateFormat', require('handlebars-dateformat'));
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+    res.render('index')
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
+console.log('listening on port' + PORT);
--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 try 
 {
-    const client = mongoose.connect('mongodb+srv://group1:group1@web-enterprise.bn7jz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    const client = mongoose.connect('mongodb+srv://group1:group1sieucappro@web-enterprise.bn7jz.mongodb.net/WEB?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         // useFindAndModify: true
     })
