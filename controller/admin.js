cbd4ea0c07f8f2efecca701e7d34880b90042257
Nguyen Thi Lan Anh <81799232+lananh20111999@users.noreply.github.com>
2022-02-28 20:01:42 +0700

    ok

--- controller/admin.js
+++ controller/admin.js
@@ -1,5 +1,20 @@
 const Account = require('../models/user');
+const staff = require('../models/staff');
+const QAcoordinator = require('../models/QAcoordinator');
+const QAmanager = require('../models/QAmanager');
 const bcrypt = require('bcryptjs');
 exports.getAdmin = async (req, res) => {
     res.render('admin/admin', { loginName: req.session.email })
+}
+
+//view all QAmanager
+exports.viewQAmanager = async (req, res) => {
+    let listQAmanager = await trainer.find();
+    res.render('admin/viewQAmanager', { listQAmanager: listQAmanager, loginName: req.session.email })
+}
+
+//view all QAcoordinator
+exports.viewQAcoordinator = async (req, res) => {
+    let listQAcoordinator = await staff.find();
+    res.render('admin/viewQAcoordinator', { listQAcoordinator: listQAcoordinator, loginName: req.session.email })
 }
--- routes/admin.js
+++ routes/admin.js
@@ -5,5 +5,10 @@ const mongoose = require('mongoose');
 const adminController = require('../controller/admin');
 const { isAdmin } = require("../middleware/auth");
 
-router.get('/admin', isAdmin, adminController.getAdmin);
+router.get('/admin', adminController.getAdmin);
+
+router.get('/admin/ViewQualityAssuranceManager', adminController.viewQAmanager);
+
+router.get('admin/ViewQualityAssuranceCoordinator', adminController.viewQAcoordinator);
+
 module.exports = router;
