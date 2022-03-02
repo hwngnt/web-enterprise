11bd6ab9720daa55afc4fe34733760ebd62f11d5
hwngnt <hwng.nt@outlook.com>
2022-03-02 22:35:27 +0700

    update

--- routes/qam.js
+++ routes/qam.js
@@ -7,6 +7,9 @@ const { isQAM } = require("../middleware/auth");
 
 // router.get('/qam_index', isQAM, qamController.getQAM);
 router.get('/qam_index', qamController.getQAM);
-router.get('/qamAddCategory', qamController.getQAMAddCategory);
-router.post('/qam/doAddCategory', qamController.doQAMAddCategory);
+router.get('/qam/qamAddCategory', qamController.getAddCategory);
+router.post('/qam/doAddCategory', qamController.doAddCategory);
+router.get('/qam/qamViewCategory', qamController.getViewCategory);
+router.get('/qam/qamViewCategoryDetail', qamController.getCategoryDetail);
+router.get('/qam/qamDeleteCategory', qamController.deleteCategory);
 module.exports = router;
