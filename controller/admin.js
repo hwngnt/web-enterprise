fea06ab13ab9305b354fb26f65432effa933209d
Nguyen Thi Lan Anh <81799232+lananh20111999@users.noreply.github.com>
2022-04-13 18:50:28 +0700

    ok

--- controller/admin.js
+++ controller/admin.js
@@ -3,13 +3,60 @@ const Staff = require('../models/staff');
 const QAcoordinator = require('../models/QAcoordinator');
 const QAmanager = require('../models/QAmanager');
 const category = require('../models/category');
+const Comments = require('../models/comments');
 const idea = require('../models/ideas');
 const validation = require('./validation');
 const bcrypt = require('bcryptjs');
 exports.getAdmin = async (req, res) => {
     res.render('admin/admin', { loginName: req.session.email })
 }
+exports.changePassword = async (req, res) => {
+    res.render('admin/changePassword', { loginName: req.session.email })
+}
+exports.doChangePassword = async (req, res) => {
+    let user = await Account.findOne({ email: req.session.email });
+    let current = req.body.current;
+    let newpw = req.body.new;
+    let confirm = req.body.confirm;
+    let errors = {};
+    let flag = true;
+    try {
+        await bcrypt.compare(current, user.password)
+            .then((doMatch) => {
+                if (doMatch) {
+                    if (newpw.length < 8) {
+                        flag = false;
+                        Object.assign(errors, { length: "Password must contain 8 characters or more!" });
+                    }
+                    else if (newpw != confirm) {
+                        flag = false;
+                        Object.assign(errors, { check: "New Password and Confirm Password do not match!" });
+                    }
+                }
+                else {
+                    flag = false;
+                    Object.assign(errors, { current: "Old password is incorrect!" });
+                }
+            });
+        if (!flag) {
+            res.render('admin/changePassword', { errors: errors, loginName: req.session.email })
+        }
+        else {
+            await bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newpw, salt, (err, hash) => {
+                    if (err) throw err;
+                    user.password = hash;
+                    user = user.save();
+                    req.session.user = user;
+                    res.redirect('/admin')
+                })
+            })
 
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
 //QAmanager
 exports.viewQAmanager = async (req, res) => {
     let listQAmanager = await QAmanager.find();
@@ -312,8 +359,9 @@ exports.deleteStaff = async (req, res) => {
         else
             console.log('Account is deleted');
     })
+    await idea.deleteMany({'author': aStaff.id});
+    await Comments.deleteMany({'author': aStaff.id});
     await Staff.findByIdAndRemove(id).then(data = {});
-
     res.redirect('/admin/viewStaff');
 }
 exports.searchStaff = async (req, res) => {
